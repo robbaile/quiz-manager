574f1887957a7363ff04b9f42c4e3da408b2a91d
robbaile <robbaile12@outlook.com>
2020-07-02 10:59:41 +0100

    Added Simple Login Class and tested

---
+++ QuizManager/QuizManager.Test/Authentication/LoginUserTests.cs
@@ -0,0 +1,44 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+using QuizManager.Authentication;
+using QuizManager.Data;
+using QuizManager.Interfaces;
+
+namespace QuizManager.Tests.Authentication
+{
+    [TestFixture]
+    public class LoginUserTests
+    {
+        private IUserService _userService;
+        private LoginUser _loginUser;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userService = A.Fake<IUserService>();
+            _loginUser = new LoginUser(_userService);
+        }
+
+        [Test]
+        public void LoginUserWithCorrectDetailsReturnsTrue()
+        {
+            // Arrange
+            A.CallTo(() => _userService.Authenticate("rob", "rob"))
+                .Returns(new User
+                {
+                    Id = 1,
+                    Username = "rob",
+                    IsEditor = true,
+                    IsViewer = true,
+                    IsRestricted = true
+                });
+
+            // Act 
+            var actual = _loginUser.AuthenticateUser("rob", "rob");
+
+            // assert 
+            actual.Should().BeTrue();
+        }
+    }
+}
---
+++ QuizManager/QuizManager/Authentication/LoginUser.cs
@@ -0,0 +1,32 @@
+﻿using QuizManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizManager.Authentication
+{
+    public class LoginUser : ILoginUser
+    {
+        private IUserService _userService;
+
+        public LoginUser(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public bool AuthenticateUser(string username, string password)
+        {
+            var user = _userService.Authenticate(username, password).GetAwaiter().GetResult();
+
+            if(user.Username == null)
+            {
+                return false;
+            }
+
+            // setup session
+
+            return true;
+        }
+    }
+}
