7e3d61204b83a96194aeb651ee3a1864bc375ddf
robbaile <robbaile12@outlook.com>
2020-07-03 08:18:34 +0100

    Added Gulp and Sass to project

---
+++ QuizManager/QuizManager/Gulpfile.js
@@ -0,0 +1,8 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', () => {
+  return gulp.src('Content/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' })) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('wwwroot/css'))
+});
